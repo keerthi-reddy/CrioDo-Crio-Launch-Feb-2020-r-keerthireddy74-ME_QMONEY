f91ed42d56c66a785ddcdc7b040ad7e45bc5ab29
keerthi-reddy <r.keerthireddy74@gmail.com>
2020-02-21 16:48:11 +0000

    ME_QMONEY_MODULE_JSON_PARSING

--- qmoney/src/test/java/com/crio/warmup/stock/PortfolioManagerApplicationTest.java
+++ qmoney/src/test/java/com/crio/warmup/stock/PortfolioManagerApplicationTest.java
@@ -1,11 +1,9 @@
 
 package com.crio.warmup.stock;
 
-import com.crio.warmup.stock.dto.PortfolioTrade;
-import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,21 +11,16 @@ class PortfolioManagerApplicationTest {
 
   @Test
   void mainReadFile() throws Exception {
-    //given
+    // given
     String filename = "trades.json";
-    List<String> expected = Arrays.asList(new String[]{"AAPL", "MSFT", "GOOGL"});
+    List<String> expected = Arrays.asList(new String[] { "AAPL", "MSFT", "GOOGL" });
 
-    //when
-    List<String> results = PortfolioManagerApplication
-        .mainReadFile(new String[]{filename});
+    // when
+    List<String> results = PortfolioManagerApplication.mainReadFile(new String[] { filename });
 
-    //then
+    // then
     Assertions.assertEquals(expected, results);
   }
-
-
-
-
 
   @Test
   public void testDebugValues() {
@@ -36,4 +29,3 @@ class PortfolioManagerApplicationTest {
   }
 
 }
-
